680dde8a28775ec489f0b7aeb5137121a7519499
Beregszászi István <beistvan@gmail.com>
2023-06-21 06:46:39 +0200

    Insert Into Table Multiple Values

---
+++ insert-into-table-multiple-values.sql
@@ -0,0 +1,6 @@
+INSERT INTO movies (id, title, release_year, genre, rating) VALUES
+  (1, 'The Shawshank Redemption', 1994, 'Drama', 9),
+  (2, 'The Godfather', 1972, 'Crime', 9),
+  (3, 'The Godfather: Part II', 1974, 'Crime', 9),
+  (4, 'The Dark Knight', 2008, 'Action', 8),
+  (5, '12 Angry Men', 1957, 'Drama', 8);
